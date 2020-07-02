f97e2de14310d5f700d25b1ad1381aebe4daeb91
caoyongxi <caoyongxi@daojia-inc.com>
2020-07-02 17:11:26 +0800

    add broadcast

---
+++ sty.js
@@ -0,0 +1,16 @@
+/**
+ *  小程序常规问题总结
+
+// 列如登录就是典型的用法
+1、跨页面间的事件传输？
+    1、采用事件广播的方式 可以参考broadcast.js
+    // https://github.com/WebReflection/broadcast
+    // https://github.com/alexgibson/notify.js
+    // https://github.com/webpack/tapable.git
+
+2、跨页面间的数据通信？
+    2、数据通过globalData形式也是可以的
+
+
+ */
+
